52e16bab9ffe04f3967bd810d4fe330b1a30f681
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2022-07-21 12:51:37 +0700

    Adding captchav3 function bypass
    Copy from https://github.com/xcscxr/Recaptcha-v3-bypass

--- lk21/extractors/__init__.py
+++ lk21/extractors/__init__.py
@@ -168,6 +168,27 @@ class BaseExtractor:
         text = raw.text if hasattr(raw, "text") else raw
         return bs4.BeautifulSoup(text, "html.parser")
 
+    def recaptcha3(self, capcay3: str) -> str:
+        """
+        For Bypass recaptchav3
+        """
+        url_base = 'https://www.google.com/recaptcha/'
+        post_data = "v={}&reason=q&c={}&k={}&co={}"
+        client = requests.Session()
+        client.headers.update({
+            'content-type': 'application/x-www-form-urlencoded'
+        })
+        matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', capcay3)[0]
+        url_base += matches[0]+'/'
+        params = matches[1]
+        res = client.get(url_base+'anchor', params=params)
+        token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+        params = dict(pair.split('=') for pair in params.split('&'))
+        post_data = post_data.format(params["v"], token, params["k"], params["co"])
+        res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
+        answer = re.findall(r'"rresp","(.*?)"', res.text)[0]    
+        return answer
+
     def getPath(self, url: str) -> str:
         """
         undocumented
